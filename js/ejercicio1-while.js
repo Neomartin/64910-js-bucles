e784d61aa61fa7d1a974be01c5ff27b3cd2a0163
Martin Rodriguez <neomartinr@gmail.com>
2023-09-20 21:54:28 -0300

    Ejercicio while 1 resuelto

---
+++ js/ejercicio1-while.js
@@ -0,0 +1,39 @@
+// #Ejercicio 1
+// Solicitar al usuario un número a través de un prompt
+// empezando desde 1 e ir incrementando este valor de a 1
+// A medida que recorremos todos los números enteros anterior e incluído el ingresado por el usuario quiero hacer un log de aquellos números que son multiplos de 7
+// El log debe ser el siguiente
+// *El número X es multiplo de 7
+
+
+const numeroIngresado = parseInt( prompt("Ingrese un número para calcular sus múltiplos") )
+let numero =  1;
+
+while(  numero <= numeroIngresado   ) {
+    
+    if( numero % 7 === 0  ) {
+        console.log(`El número ${numero} es multiplo de 7`)
+    }
+
+    numero++
+}
+
+
+
+
+
+
+
+
+
+
+// #Ejercicio 2
+/*
+    - Solicitar a una maestra que ingresa las calificaciones de sus alumnos
+    - Pedir constantemente las notas hasta que la maestra ingrese un 0 como valor En ese momento saldremos de nuestro bucle e imprimiremos el promedio del curso según las notas ingresadas/     
+    
+    !Tener en cuenta que cuando presione 0 no afecte al promedio de notas.
+
+    NO ESTA BIEN: ( 10 + 10 + 10 + 0 ) /  4 = 7,5
+    #ESTA BIEN: ( 10 + 10 + 10) /  3 = 10
+*/ 
--- js/while.js
+++ js/while.js
@@ -1,1 +1,13 @@
-console.log(`Archivo de bucle while cargado`)
+let counter = 1;
+
+while(  counter <= 10   ) {
+    //Líneas de código a repetir según las vecees que iteremos 
+
+    console.log(`Estamos contando ${counter}`);
+
+    counter = counter + 1;
+
+}
+
+
+console.log(`Paso el while`)
